bd7a1373591beef55359f566cfe4e8e789ea4ef9
Mike Farrow <mike@farrow.io>
2025-11-20 11:50:23 +0000

    removing react-timing-hooks for now

--- src/components/general/progress.tsx
+++ src/components/general/progress.tsx
@@ -1,15 +1,16 @@
 'use client';
 
 import clsx from 'clsx';
-import { useAnimationFrameLoop } from 'react-timing-hooks';
-import React, { useEffect, useState } from 'react';
+
+// import { useAnimationFrameLoop } from 'react-timing-hooks';
+// import React, { useEffect, useState } from 'react';
 
 export interface Props extends React.ComponentPropsWithoutRef<'div'> {
   className?: string;
   min?: number;
   max?: number;
   current: number;
-  animate?: boolean;
+  // animate?: boolean;
 }
 
 export default function Progress({
@@ -17,30 +18,31 @@ export default function Progress({
   min = 0,
   max = 100,
   current,
-  animate,
+  // animate,
   ...rest
 }: Props) {
-  const [inc, setInc] = useState(0);
+  // const [inc, setInc] = useState(0);
 
-  const { start, stop } = useAnimationFrameLoop(() => {
-    setInc(current === 0 ? 0 : inc + (current - inc) / 10);
-  });
+  // const { start, stop } = useAnimationFrameLoop(() => {
+  //   setInc(current === 0 ? 0 : inc + (current - inc) / 10);
+  // });
 
-  useEffect(() => {
-    if (animate) {
-      start();
-    } else {
-      stop();
-    }
-  }, [animate, start, stop]);
+  // useEffect(() => {
+  //   if (animate) {
+  //     start();
+  //   } else {
+  //     stop();
+  //   }
+  // }, [animate, start, stop]);
 
   const percent =
     min === max
       ? 0
-      : ((Math.min(max, Math.max(animate === undefined ? current : inc, min)) -
-          min) /
-          (max - min)) *
-        100;
+      : ((Math.min(max, Math.max(current, min)) - min) / (max - min)) * 100;
+  // : ((Math.min(max, Math.max(animate === undefined ? current : inc, min)) -
+  //     min) /
+  //     (max - min)) *
+  //   100;
 
   return (
     <div
